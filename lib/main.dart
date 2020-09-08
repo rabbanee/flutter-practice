1b6e8538d8c78bf6197a7eb09033fa1d32abc7c2
rabbanee <daffarabbanee@gmail.com>
2020-09-08 11:35:34 +0700

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FirstPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstPage(),
+        '/second': (context) => SecondPage(),
+        '/third': (context) => ThirdPage(),
+      },
     );
   }
 }
@@ -24,11 +29,21 @@ class FirstPage extends StatelessWidget {
           children: <Widget>[
             RaisedButton(
               child: Text('Go to Second Page'),
-              onPressed: () {},
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => SecondPage(),
+                //   ),
+                // );
+                Navigator.pushNamed(context, '/second');
+              },
             ),
             RaisedButton(
-              child: Text('Go to Third Page'),
-              onPressed: () {},
+              child: Text('Back to Third Page'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
@@ -50,11 +65,57 @@ class SecondPage extends StatelessWidget {
           children: <Widget>[
             RaisedButton(
               child: Text('Go to Third Page'),
-              onPressed: () {},
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => ThirdPage(),
+                //   ),
+                // );
+                Navigator.pushNamed(context, '/third');
+              },
             ),
             RaisedButton(
-              child: Text('Go to First Page'),
-              onPressed: () {},
+              child: Text('Back to First Page'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Back to First Page'),
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => FirstPage(),
+                //   ),
+                // );
+                Navigator.pushNamed(context, '/');
+              },
+            ),
+            RaisedButton(
+              child: Text('Back to Second Page'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
